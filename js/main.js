23828548ba6dd4a794d3ca0f0eae9d75b4ee07be
alex <alejandro.barrio@atos.net>
2019-06-26 16:05:17 +0200

    new theme

---
+++ js/main.js
@@ -0,0 +1,33 @@
+
+(function ($) {
+  Drupal.behaviors.myToggle = {
+    attach: function(context, settings) {
+        // var $body = $('#block-trialobjectivesinstructions');
+        // $($body).hide();
+        $('#region-help').hide();
+        
+      $('#btn-help, #user-menu .help', context).css("cursor","pointer").click(function() {
+        $(this).toggleClass('active');
+        
+                event.preventDefault();
+         
+         $('#region-help').toggleClass('active').toggle('500');
+        // $(this).parent().toggleClass('active');
+        // $($body).toggle('slow');
+      });
+     
+      var theHidden;
+      var myFeatured;
+      $('#block-reboot-content .view-duplicated .view-content > div').each(function(index, value){
+        
+        myFeatured = theHidden;
+        theHidden = $(this)[0].classList[0];
+        
+        if(theHidden == myFeatured){ 
+          $(this).hide();	
+        }
+      });
+      
+    }
+  };
+})(jQuery);
